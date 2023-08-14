63790b0cef533dc5c1398c646d8e4203edfe5516
Erick_Dev <erickhenrique.rg@gmail.com>
2023-08-14 19:30:40 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,117 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Ola Digite seu nome:");
+//string nome = Console.ReadLine()!;
+//Console.WriteLine("Seja Bem vindo " + nome);//
+
+
+//== Questão 2 //==
+
+//double[] nota = new double[3];
+//double soma = 0;
+
+//for (int i = 0; i <= nota.Length -1 ; i++)
+//{
+
+//Console.WriteLine("digite a nota " + i);
+//    nota[i]= Convert.ToDouble(Console.ReadLine());
+//}
+//for (int i = 0; i < nota.Length; i++)
+//{
+//soma += nota[i];
+//}
+
+//double media = soma / (double)nota.Length;
+
+//Console.WriteLine("a Media de Nota Dos alunos é :" + media);
+
+
+
+//=== QUESTAO 3 //===
+
+
+
+//Console.WriteLine("Digite Um Numero: ");
+
+//double number = Convert.ToDouble(Console.ReadLine());
+
+//if (number % 2 == 0) 
+//{
+//Console.WriteLine("O numero é par");
+//}
+//else
+//{
+//Console.WriteLine("o Número é impar ");
+//}
+
+
+//=== QUESTAO 4 //===
+
+
+//int n = 10;
+//double primeiroTermo = 0;
+//double SegundoTermo = 1;
+
+//Console.WriteLine("Os primeiros " + n + " números da sequência de Fibonacci são:");
+
+
+//for (int i = 0; i < n; i++)
+//{
+//Console.WriteLine(primeiroTermo + "");
+
+//double proximoTermo = primeiroTermo + SegundoTermo;
+//primeiroTermo = SegundoTerm   
+//SegundoTermo = proximoTermo;
+
+//}
+
+
+
+
+//==== QUESTAO 5 =======//
+
+
+//double[] number = new double[5]++;
+//double maior = 0;
+//double menor = 100000000000000;
+
+//Console.WriteLine("Insira 5 numeros ");
+//for (int i = 0; i <= number.Length -1 ; i++)
+//{
+
+
+//number[i]= Convert.ToDouble(Console.ReadLine());
+
+//if (number[i] > maior)
+//{
+//maior = number[i];
+
+
+// }
+//if (number[i] < menor)
+//
+//menor = number[i];
+//  }
+
+
+//}
+//Console.WriteLine("O maior numero digitado foi: " + maior);
+//Console.WriteLine("O menor numero digitado foi: " + menor);
+
+
+
+//==== QUESTAO 6 =======//
+
+
+
+
+//Console.WriteLine("Digite O numero que deseja Saber");
+//int num = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine($"O Fatorial de{num} é {fatorial(num)}");
+//static int fatorial(int num)
+//{
+ //   if (num == 0)
+   // {
+     //   return 1;
+    //}
+   // else return num * fatorial(num - 1);
+//};    
